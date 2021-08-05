22b317a55d77438383c896cb3feac2cedbcb3952
Koffi Cobbin <52169278+Koffi-Cobbin@users.noreply.github.com>
2021-08-05 11:40:28 +0000

    Delete src1 directory

--- src1/models/regions/region.py
+++
@@ -1,7 +0,0 @@
-
-
-class Region():
-    def __init__(self, name, towns, _id=None):
-        self._id = None if _id is None else _id
-        self.name = name 
-        self.towns = [] #A list of town ids in this region
--- src1/models/towns/town.py
+++
@@ -1,6 +0,0 @@
-
-class Town():
-    def __init__(self, name, polylines, _id=None):
-        self._id = None if _id is None else _id
-        self.name = name 
-        self.polylines = [] #A list of polyline ids in this town
