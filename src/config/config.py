6a49076276588ce50c8373a6e6e14ef3a3b5a33a
Koffi Cobbin <52169278+Koffi-Cobbin@users.noreply.github.com>
2021-08-07 20:27:30 +0000

    Update config.py

--- src/config/config.py
+++ src/config/config.py
@@ -3,9 +3,9 @@ from flask import Flask
 from flask_mysqldb import MySQL 
 
 app = Flask(__name__)
-app.secret_key = app.secret_key = "1234" #os.environ.get("SECRETE_KEY")
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'argis'
+app.secret_key = app.secret_key = os.environ.get("SECRETE_KEY")
+app.config['MYSQL_HOST'] = os.environ.get("MYSQL_HOST") 
+app.config['MYSQL_USER'] = os.environ.get("MYSQL_USER")
+app.config['MYSQL_PASSWORD'] = os.environ.get("MYSQL_PASSWORD")
+app.config['MYSQL_DB'] = os.environ.get("MYSQL_DB")
 mysql = MySQL(app)
